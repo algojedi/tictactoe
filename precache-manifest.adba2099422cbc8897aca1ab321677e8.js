b43984b6f1c4114befdc778ea1be783ebfe491f3
algojedi <vicbanwait@hotmail.com>
2019-06-12 12:52:32 -0400

    Updates

---
+++ precache-manifest.adba2099422cbc8897aca1ab321677e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df5deb9b1c141530cd8304c23434940",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "7aaa9e2383d29b243895",
+    "url": "/tictactoe/static/css/main.db4e1eff.chunk.css"
+  },
+  {
+    "revision": "20d1bb46ff6c583b9b3b",
+    "url": "/tictactoe/static/js/2.5fd1c813.chunk.js"
+  },
+  {
+    "revision": "7aaa9e2383d29b243895",
+    "url": "/tictactoe/static/js/main.a7061b4b.chunk.js"
+  },
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  }
+]);
